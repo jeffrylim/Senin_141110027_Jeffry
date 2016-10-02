af8063b3769e2111a87e11b056eb314e3815fa27
Teddy <teddychien2708@gmail.com>
2016-10-02 15:10:51 +0700

    Tugas

---
+++ Senin_141110027_Jeffry/Latihan_1_2/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Latihan_1_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Logo = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.Logo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Logo
+            // 
+            this.Logo.Controls.Add(this.tabPage1);
+            this.Logo.Controls.Add(this.tabPage2);
+            this.Logo.ImageList = this.imageList1;
+            this.Logo.Location = new System.Drawing.Point(93, 84);
+            this.Logo.Name = "Logo";
+            this.Logo.Padding = new System.Drawing.Point(10, 5);
+            this.Logo.SelectedIndex = 0;
+            this.Logo.Size = new System.Drawing.Size(335, 236);
+            this.Logo.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.ImageKey = "12.jpg";
+            this.tabPage1.Location = new System.Drawing.Point(4, 27);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(327, 205);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Logo";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.ImageKey = "logo.png";
+            this.tabPage2.Location = new System.Drawing.Point(4, 27);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(327, 205);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Logo 2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "12.jpg");
+            this.imageList1.Images.SetKeyName(1, "logo.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 428);
+            this.Controls.Add(this.Logo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Logo.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl Logo;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
