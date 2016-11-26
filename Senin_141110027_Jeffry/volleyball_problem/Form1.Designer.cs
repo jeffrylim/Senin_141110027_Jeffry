03cc9c5cb74087bb14725e9fdcb248b20d7488e0
jeffrylim <jeffryhau@gmail.com>
2016-11-26 19:17:17 +0700

    volleyball_problem

---
+++ Senin_141110027_Jeffry/volleyball_problem/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace volleyball_problem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtHasil = new System.Windows.Forms.TextBox();
+            this.BtnHitung = new System.Windows.Forms.Button();
+            this.Txt2 = new System.Windows.Forms.TextBox();
+            this.Txt1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(249, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Hasil";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Nilai B";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Nilai A";
+            // 
+            // TxtHasil
+            // 
+            this.TxtHasil.Location = new System.Drawing.Point(249, 62);
+            this.TxtHasil.Name = "TxtHasil";
+            this.TxtHasil.Size = new System.Drawing.Size(100, 20);
+            this.TxtHasil.TabIndex = 10;
+            // 
+            // BtnHitung
+            // 
+            this.BtnHitung.Location = new System.Drawing.Point(147, 62);
+            this.BtnHitung.Name = "BtnHitung";
+            this.BtnHitung.Size = new System.Drawing.Size(75, 23);
+            this.BtnHitung.TabIndex = 9;
+            this.BtnHitung.Text = "Hitung";
+            this.BtnHitung.UseVisualStyleBackColor = true;
+            this.BtnHitung.Click += new System.EventHandler(this.BtnHitung_Click);
+            // 
+            // Txt2
+            // 
+            this.Txt2.Location = new System.Drawing.Point(22, 85);
+            this.Txt2.Name = "Txt2";
+            this.Txt2.Size = new System.Drawing.Size(100, 20);
+            this.Txt2.TabIndex = 8;
+            // 
+            // Txt1
+            // 
+            this.Txt1.Location = new System.Drawing.Point(22, 40);
+            this.Txt1.Name = "Txt1";
+            this.Txt1.Size = new System.Drawing.Size(100, 20);
+            this.Txt1.TabIndex = 7;
+            this.Txt1.TextChanged += new System.EventHandler(this.Txt1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 134);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtHasil);
+            this.Controls.Add(this.BtnHitung);
+            this.Controls.Add(this.Txt2);
+            this.Controls.Add(this.Txt1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtHasil;
+        private System.Windows.Forms.Button BtnHitung;
+        private System.Windows.Forms.TextBox Txt2;
+        private System.Windows.Forms.TextBox Txt1;
+    }
+}
+
---
+++ Senin_141110027_Jeffry/volleyball_problem/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace volleyball_problem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnHitung_Click(object sender, EventArgs e)
+        {
+            long a, b, hasil = 1, temp;
+
+            a = Convert.ToInt32(Txt1.Text);
+            b = Convert.ToInt32(Txt2.Text);
+
+            if (a < b)
+            {
+                temp = a;
+                a = b;
+                b = temp;
+            }
+
+            if (((a == 25) && (b < 25 && b > 0)) || ((b == 25) && (a < 25 && a > 0)) || (a > 25 && (a - b == 2)))
+            {
+                if ((a + b - 1) < 47)
+                {
+                    hasil = comb(a + b - 1, a - 1);
+                    hasil *= pow(2, a - 25);
+                    hasil %= mod;
+                }
+                else
+                {
+                    hasil = comb(47, 24);
+                    hasil *= pow(2, a - 25);
+                    hasil %= mod;
+                }
+
+            }
+            else
+                hasil = 0;
+
+            TxtHasil.Text = hasil.ToString();
+        }
+
+        const long mod = 1000000007;
+        public static long fakto(long a)
+        {
+            long akhir = 1;
+            for (long i = a; i > 1; i--)
+            {
+                akhir *= i;
+                akhir %= mod;
+            }
+            return akhir;
+        }
+
+        public static long pow(long a, long b)
+        {
+            long akhir = 1;
+            while (b > 0)
+            {
+                if (b % 2 == 1)
+                {
+                    akhir = (akhir * a) % mod;
+                }
+                a = (a * a) % mod;
+                b /= 2;
+            }
+            return akhir;
+        }
+
+        public static long invers(long n)
+        {
+            return pow(n, mod - 2);
+        }
+
+        public static long comb(long a, long b)
+        {
+            long hasil = 1;
+            hasil *= fakto(a);
+            hasil %= mod;
+            hasil *= invers(fakto(a - b));
+            hasil %= mod;
+            hasil *= invers(fakto(b));
+            hasil %= mod;
+            return hasil;
+        }
+
+        private void Txt1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
